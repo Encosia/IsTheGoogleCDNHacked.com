3c392fafc0065ad57c5e5bff505598046a23317f
Dave Ward <dave@encosia.com>
2014-02-13 18:51:02 -0500

    Initial

---
+++ models/cdnCompare.js
@@ -0,0 +1,64 @@
+var request = require('request');
+
+var urls = {
+	googleCdn: {
+		debug: 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js',
+		production: 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'
+	},
+	official: {
+		debug: 'http://code.jquery.com/jquery-1.11.0.js',
+		production: 'http://code.jquery.com/jquery-1.11.0.min.js'
+	}
+}
+
+var cachedLookup;
+
+function compare(officialCopy, cdnCopy) {
+	// Remove newlines to avoid meaningless false positives.
+	officialCopy = officialCopy.replace(/\n/g, '');
+	cdnCopy = cdnCopy.replace(/\n/g, '');
+
+  // Spaces are also meaningless for our purposes here, but
+  //  the difference between ANSI and UTF-8 spaces causes inequality.
+  officialCopy = officialCopy.replace(/\s/g, '');
+  cdnCopy = cdnCopy.replace(/\s/g, '');
+	
+	return officialCopy === cdnCopy;
+}
+
+function performComparison(callback) {
+  request(urls.googleCdn.production, function(error, response, body) {
+    var cdn = body;
+
+    request(urls.official.production, function(error, response, body) {
+      var official = body;
+
+      var status = compare(official, cdn);
+
+      if (status)
+        status = { result: false, verdict: 'Nope' };
+      else
+        status = { result: true, verdict: 'Maybe' };
+
+      status.lastCheckedAt = new Date();
+
+      cachedLookup = status;
+
+      // Refresh the cached comparison in 5 minutes.
+      setTimeout(performComparison, 1000 * 60 * 5);
+
+      if (typeof callback == 'function')
+        callback(status);
+    });
+  });
+}
+
+exports.isTheGoogleCDNHacked = function(callback) {
+  if (cachedLookup) {
+    callback(cachedLookup);
+  } else {
+    performComparison(function() {
+      callback(cachedLookup);
+    });
+  }
+};
